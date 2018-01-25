ee801ed59747b2b1cd8c48207c622362d222200b
Ben Hohnke <ben.hohnke@nttict.com>
2018-01-25 13:48:27 +1100

    Move to cherrypy

--- test_webapp.py
+++ test_webapp.py
@@ -2,4 +2,5 @@ import webapp
 
 
 def test_hello_world():
-    assert 'Hello World!11 test auto webhook' == webapp.hello_world()
+    web_object = webapp.TestSite()
+    assert 'TEST TEST TEST' == web_object.index()
